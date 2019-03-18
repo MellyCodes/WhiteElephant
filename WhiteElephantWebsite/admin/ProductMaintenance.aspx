74a695947a0eb26debe24b42bc6566ead656deac
COFFEEISLIFE\melan <melanieroyplommer@gmail.com>
2019-03-18 09:41:29 -0300

    Added Product Maintenance - no MasterPage

---
+++ WhiteElephantWebsite/admin/ProductMaintenance.aspx
@@ -0,0 +1,141 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/My.Master" AutoEventWireup="true" CodeBehind="ProductMaintenance.aspx.cs" Inherits="WhiteElephantWebsite.admin.ProductMaintenance" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder5" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+        <div style="padding-bottom: 20px;">
+        <asp:Label ID="lblError" runat="server" Text="" ForeColor="Red"></asp:Label>
+        <asp:Label ID="lblMessage" runat="server" Text="" ForeColor="Green"></asp:Label>
+        <asp:GridView ID="grdProducts" runat="server" AutoGenerateColumns="False"
+            DataKeyNames="id"
+            OnRowCommand="grdProducts_RowCommand"
+            OnRowDeleting="grdProducts_RowDeleting"
+            OnRowUpdating="grdProducts_RowUpdating"
+            OnRowEditing="grdProducts_RowEditing"
+            OnRowCancelingEdit="grdProducts_RowCancelingEdit"
+            AllowPaging="True"
+            OnPageIndexChanging="grdProducts_PageIndexChanging"
+            PageSize="5"
+            ShowFooter="True"
+            AllowSorting="true"
+            >
+            <FooterStyle BackColor="#3399ff" />
+            <Columns>
+                <asp:TemplateField HeaderText="Id">
+                    <ItemTemplate>
+                        <asp:Label ID="lblProductId" runat="server" Text='<%# Eval("id") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Product Name">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtProductName" runat="server" Text='<%# Eval("name") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblProductName" runat="server" Text='<%# Eval("name") %>'></asp:Label>
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtProductNameNew" runat="server"></asp:TextBox>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Brief Description">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtBriefDesc" runat="server" Text='<%# Eval("briefDescription") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblBriefDesc" runat="server" Text='<%# Eval("briefDescription") %>'></asp:Label>
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtBriefDescNew" runat="server"></asp:TextBox>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Full Description">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtFullDesc" runat="server" Text='<%# Eval("fullDescription") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblFullDesc" runat="server" Text='<%# Eval("fullDescription") %>'></asp:Label>
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtFullDescNew" runat="server"></asp:TextBox>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Active">
+                    <EditItemTemplate>
+                        <asp:CheckBox ID="chkStatus" runat="server" Checked='<%# Bind("statusCode") %>' />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:CheckBox ID="chkStatusNew" runat="server" />
+                    </FooterTemplate>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="chkStatusDisplay" runat="server" Checked='<%# Bind("statusCode") %>' Enabled="False" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Price">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtPrice" runat="server" Text='<%# Eval("price", "{0:0.00}") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblPrice" runat="server" Text='<%# Eval("price", "{0:c}") %>'></asp:Label>
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtPriceNew" runat="server"></asp:TextBox>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Featured">
+                    <EditItemTemplate>
+                        <asp:CheckBox ID="chkFeatured" runat="server" Checked='<%# Bind("featured") %>' />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:CheckBox ID="chkFeaturedNew" runat="server" />
+                    </FooterTemplate>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="chkFeaturedDisplay" runat="server" Checked='<%# Bind("featured") %>' Enabled="False" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Category">
+                    <EditItemTemplate>
+                        <asp:DropDownList ID="ddlCategories" runat="server"></asp:DropDownList>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblCategory" runat="server" Text='<%# Eval("CategoryName") %>'></asp:Label>
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        <asp:DropDownList ID="ddlCategoriesNew" runat="server"></asp:DropDownList>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Image">
+                    <ItemTemplate>
+                        <asp:Image ID="imgProduct" runat="server" ImageUrl='<%# "~/" + Eval("ImageName") %>' />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:FileUpload ID="uplUpdateImage" runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:FileUpload ID="uplNewImage" runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField ShowHeader="False">
+                    <EditItemTemplate>
+                        <asp:Button ID="btnUpdate" runat="server" CausesValidation="True" CommandName="Update"
+                            Text="Update" />&nbsp;<asp:Button ID="btnCancel" runat="server" CausesValidation="False"
+                                CommandName="Cancel" Text="Cancel" />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Button ID="btnEdit" runat="server" CausesValidation="False" CommandName="Edit"
+                            Text="Edit" />&nbsp;<asp:Button ID="Button2" runat="server" CausesValidation="False"
+                                CommandName="Delete" Text="Delete" />
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        <asp:Button ID="btnNew" runat="server" CommandName="btnNew" Text="Insert New Product" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+            </Columns>
+
+        </asp:GridView>
+    </div>
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="ContentPlaceHolder4" runat="server">
+</asp:Content>
---
+++ WhiteElephantWebsite/admin/ProductMaintenance.aspx.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WhiteElephantWebsite.admin
+{
+    public partial class ProductMaintenance : AdminPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+            base.Page_Load(sender, e);
+            lblMessage.Text = "";
+
+            if (!IsPostBack)
+            {
+                LoadProductsGridView();
+
+            }
+        }
+
+        private void LoadProductsGridView()
+        {
+            DBHelper.DataBindingWithPaging(this.grdProducts, "SelectProductMaintenance");
+            //Load the dropdown list in the create row
+            DropDownList ddlCategoriesInFooter = (DropDownList)(this.grdProducts.FooterRow.FindControl("ddlCategoriesNew"));
+            BindCategoryDropdownList(ddlCategoriesInFooter);
+
+        }
+
+        protected void grdProducts_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "btnNew")
+            {
+                //New product
+                AddNewProduct();
+
+                LoadProductsGridView();
+            }
+        }
+
+        protected void grdProducts_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int productId = Convert.ToInt32(this.grdProducts.DataKeys[e.RowIndex].Values[0]);
+            SqlParameter prmProductId = new SqlParameter() { ParameterName = "@ProductId", SqlDbType = SqlDbType.Int, Value = productId };
+
+            DBHelper.NonQuery("DeleteProduct", new SqlParameter[] { prmProductId });
+
+            LoadProductsGridView();
+        }
+
+        protected void grdProducts_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            List<SqlParameter> prms = new List<SqlParameter>();
+
+            TextBox txtProductName = (TextBox)this.grdProducts.Rows[e.RowIndex].FindControl("txtProductName");
+            TextBox txtBriefDesc = (TextBox)this.grdProducts.Rows[e.RowIndex].FindControl("txtBriefDesc");
+            TextBox txtFullDesc = (TextBox)this.grdProducts.Rows[e.RowIndex].FindControl("txtFullDesc");
+            TextBox txtPrice = (TextBox)this.grdProducts.FooterRow.FindControl("txtPrice");
+            CheckBox chkFeatured = (CheckBox)this.grdProducts.Rows[e.RowIndex].FindControl("chkFeatured");
+            CheckBox chkStatus = (CheckBox)this.grdProducts.Rows[e.RowIndex].FindControl("chkStatus");
+            FileUpload uplImage = (FileUpload)this.grdProducts.Rows[e.RowIndex].FindControl("uplUpdateImage");
+            DropDownList ddlCategories = (DropDownList)this.grdProducts.Rows[e.RowIndex].FindControl("ddlCategories");
+
+            try
+            {
+                string productName = txtProductName.Text.Trim();
+                string productBriefDesc = txtBriefDesc.Text.Trim();
+                string productFullDesc = txtFullDesc.Text.Trim();
+                decimal price = Convert.ToDecimal(txtPrice.Text.Trim());
+                bool featured = chkFeatured.Checked;
+                bool status = chkStatus.Checked;
+                string fileName = $"{ Server.MapPath("~/images/")}{ uplImage.FileName}";
+                string imageName = $"images/{ uplImage.FileName }";
+                string altText = uplImage.FileName;
+                int categoryId = ddlCategories.SelectedIndex > 0 ? Convert.ToInt32(ddlCategories.SelectedValue) : 0;
+                int productId = Convert.ToInt32(this.grdProducts.DataKeys[e.RowIndex].Values[0]);
+
+                uplImage.SaveAs(fileName);
+
+                prms.Add(DBHelper.SetProductIdParam(productId));
+                prms.Add(DBHelper.SetProductNameParam(productName));
+                prms.Add(DBHelper.SetProductBriefDescParam(productBriefDesc));
+                prms.Add(DBHelper.SetProductFullDescParam(productFullDesc));
+                prms.Add(DBHelper.SetProductPriceParam(price));
+                prms.Add(DBHelper.SetProductFeaturedParam(featured));
+                prms.Add(DBHelper.SetProductStatusCodeParam(status));
+
+                if (fileName != "") //Don't make changes to the current image
+                {
+                    prms.Add(DBHelper.SetProductImageName(imageName));
+                    prms.Add(DBHelper.SetProductImageAlt(altText));
+                    prms.Add(DBHelper.SetProductImageName(DateTime.Now));
+                }
+
+                prms.Add(DBHelper.SetCategoryIdParam(categoryId));
+
+                DBHelper.NonQuery("UpdateProduct", prms.ToArray());
+
+                lblMessage.Text = "Product was updated";
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+
+            LoadProductsGridView();
+        }
+
+        protected void grdProducts_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            grdProducts.EditIndex = e.NewEditIndex;
+            LoadProductsGridView();
+            Control ddlToFind = this.grdProducts.Rows[e.NewEditIndex].FindControl("ddlCategories");
+            DropDownList ddlCategory = ddlToFind != null ? (DropDownList)(ddlToFind) : null;
+
+            if (ddlCategory != null)
+            {
+                int productId = Convert.ToInt32(this.grdProducts.DataKeys[e.NewEditIndex].Values[0]);
+                SqlParameter prmProductId = new SqlParameter() { ParameterName = "@ProductId", SqlDbType = SqlDbType.Int, Value = productId };
+                int categoryId = DBHelper.GetQueryValue<int>("SELECT categoryId FROM Products WHERE id=@ProductId", "categoryId", new SqlParameter[] { prmProductId }, CommandType.Text);
+
+                BindCategoryDropdownList(ddlCategory);
+
+                ddlCategory.SelectedValue = categoryId.ToString();
+            }
+        }
+
+        private void BindCategoryDropdownList(DropDownList ddlCategory)
+        {
+            ddlCategory.DataValueField = "id";
+            ddlCategory.DataTextField = "name";
+            DBHelper.DataBinding(ddlCategory, "SelectCategories");
+            ddlCategory.Items.Insert(0, new ListItem("--Select Category--"));
+        }
+
+        protected void grdProducts_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            grdProducts.EditIndex = -1;
+            LoadProductsGridView();
+        }
+
+        protected void grdProducts_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grdProducts.PageIndex = e.NewPageIndex;
+            LoadProductsGridView();
+        }
+
+        private void AddNewProduct()
+        {
+            List<SqlParameter> prms = new List<SqlParameter>();
+
+            TextBox txtProductName = (TextBox)this.grdProducts.FooterRow.FindControl("txtProductNameNew");
+            TextBox txtBriefDesc = (TextBox)this.grdProducts.FooterRow.FindControl("txtBriefDescNew");
+            TextBox txtFullDesc = (TextBox)this.grdProducts.FooterRow.FindControl("txtFullDescNew");
+            TextBox txtPrice = (TextBox)this.grdProducts.FooterRow.FindControl("txtPriceNew");
+            CheckBox chkFeatured = (CheckBox)this.grdProducts.FooterRow.FindControl("chkFeaturedNew");
+            CheckBox chkStatus = (CheckBox)this.grdProducts.FooterRow.FindControl("chkStatusNew");
+            FileUpload uplImage = (FileUpload)this.grdProducts.FooterRow.FindControl("uplNewImage");
+            DropDownList ddlCategories = (DropDownList)this.grdProducts.FooterRow.FindControl("ddlCategoriesNew");
+
+            try
+            {
+                string productName = txtProductName.Text.Trim();
+                string productBriefDesc = txtBriefDesc.Text.Trim();
+                string productFullDesc = txtFullDesc.Text.Trim();
+                decimal price = Convert.ToDecimal(txtPrice.Text.Trim());
+                bool featured = chkFeatured.Checked;
+                bool status = chkStatus.Checked;
+                string fileName = $"{ Server.MapPath("~/images/")}{ uplImage.FileName}";
+                string imageName = $"images/{ uplImage.FileName }";
+                string altText = uplImage.FileName;
+                int categoryId = ddlCategories.SelectedIndex > 0 ? Convert.ToInt32(ddlCategories.SelectedValue) : 0;
+
+                uplImage.SaveAs(fileName);
+
+                prms.Add(DBHelper.SetProductNameParam(productName));
+                prms.Add(DBHelper.SetProductBriefDescParam(productBriefDesc));
+                prms.Add(DBHelper.SetProductFullDescParam(productFullDesc));
+                prms.Add(DBHelper.SetProductPriceParam(price));
+                prms.Add(DBHelper.SetProductFeaturedParam(featured));
+                prms.Add(DBHelper.SetProductStatusCodeParam(status));
+                prms.Add(DBHelper.SetProductImageName(imageName));
+                prms.Add(DBHelper.SetProductImageAlt(altText));
+                prms.Add(DBHelper.SetProductImageName(DateTime.Now));
+                prms.Add(DBHelper.SetCategoryIdParam(categoryId));
+
+                prms.Add(new SqlParameter()
+                {
+                    ParameterName = "@Id",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Output
+                });
+
+                int id = DBHelper.Insert<int>("InsertProduct", "@Id", prms.ToArray());
+
+                lblMessage.Text = $"Product was created. Id: {id.ToString()}";
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+        }
+    }
+}
