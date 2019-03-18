3e9fbe4896d95ab627a60ba52a076b569054684f
CPPPRINCESS\court <courtneydiotte@live.ca>
2019-03-18 09:35:24 -0300

    Added link to master page for admin login

--- WhiteElephantWebsite/My.Master.designer.cs
+++ WhiteElephantWebsite/My.Master.designer.cs
@@ -173,5 +173,14 @@ namespace WhiteElephantWebsite {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblCopyRightYear;
+        
+        /// <summary>
+        /// hypAdminLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypAdminLogin;
     }
 }
--- WhiteElephantWebsite/admin/Login.aspx.cs
+++ WhiteElephantWebsite/admin/Login.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,5 +15,74 @@ namespace WhiteElephantWebsite.admin
         {
 
         }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            e.Authenticated = false;
+            string loginUser = Login1.UserName;
+            string loginPassword = Login1.Password;
+
+            e.Authenticated = AuthenticateAdmin(loginUser, loginPassword);
+
+            if (e.Authenticated)
+            {
+                if (Request.QueryString["returnurl"] != null)
+                    Response.Redirect($"~/admin/{Request.QueryString["returnurl"]}");
+                else
+                    Response.Redirect("~/admin/AdminDashboard.aspx");
+            }
+        }
+
+        private bool AuthenticateAdmin(string userName, string password)
+        {
+            bool result = false;
+            try
+            {
+                List<SqlParameter> prms = new List<SqlParameter>();
+
+                prms.Add(new SqlParameter()
+                {
+                    ParameterName = "@Email",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 50,
+                    Value = userName
+                });
+
+                prms.Add(new SqlParameter()
+                {
+                    ParameterName = "@Password",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 50,
+                    Value = password
+                });
+
+                result = !string.IsNullOrEmpty(DBHelper.GetScalarValue<string>("LoginAdmin", prms.ToArray()));
+
+                if (result)
+                {
+                    Session["authenticated"] = true;
+                    Session["authenticatedUser"] = userName;
+                    Session["admin"] = true;
+                }
+                else
+                {
+                    Session["authenticated"] = null;
+                    Session["authenticatedUser"] = null;
+                    Session["admin"] = null;
+                }
+            }
+            catch (SqlException ex)
+            {
+                Login1.FailureText = ex.Message;
+            }
+
+            return result;
+        }
+
+        protected void Login1_LoginError(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
